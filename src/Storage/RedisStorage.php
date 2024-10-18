72f7b4f2d4e76acbeee4f0ecf5ce750d580f667f
mohachi <hachiassali15@gmail.com>
2024-10-18 16:26:21 +0100

    feat: implement redis storage
    - add tests

---
+++ src/Storage/RedisStorage.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Xwoole\Session\Storage;
+
+use OutOfBoundsException;
+use Redis;
+use RuntimeException;
+
+class RedisStorage implements Contract
+{
+    
+    public function __construct(
+        readonly Redis $dbc,
+        $host,
+        $port = 6379,
+        $timeout = 0.0,
+        $reserved = null,
+        $retryInterval = 0,
+        $readTimeout = 0.0
+    )
+    {
+        $dbc->connect($host, $port, $timeout, $reserved, $retryInterval, $readTimeout);
+    }
+    
+    public function check(string $id): bool
+    {
+        return $this->dbc->exists($id);
+    }
+    
+    public function get(string $id): array
+    {
+        if( ! $this->dbc->exists($id) )
+        {
+            throw new OutOfBoundsException;
+        }
+        
+        return unserialize($this->dbc->get($id));
+    }
+    
+    public function set(string $id, array $data): void
+    {
+        $this->dbc->set($id, serialize($data));
+    }
+    
+    public function rename(string $oldId, string $newId): void
+    {
+        $this->dbc->rename($oldId, $newId);
+    }
+    
+    public function unset(string $id): void
+    {
+        $this->dbc->del($id);
+    }
+    
+    public function close(): void
+    {
+        $this->dbc->close();
+    }
+    
+}
